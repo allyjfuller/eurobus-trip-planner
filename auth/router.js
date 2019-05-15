559e518ce597214a60b40547b7055186f1cf2ef8
allyjfuller <allyjfuller@gmail.com>
2019-05-15 09:29:59 +0200

    Jwt strategy error easy fix

---
+++ auth/router.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const config = require('../config');
+
+const createAuthToken = user => {
+	return jwt.sign({user}, config.JWT_SECRET, {
+		subject: user.username,
+		expiresIn: config.JWT_EXPIRY,
+		algorithm: 'HS256'
+	});
+};
+
+const router = express.Router();
+router.use(bodyParser.json());
+
+router.post(
+	'/login',
+	passport.authenticate('local', {session: false}),
+	(req, res) => {
+		const authToken = createAuthToken(req.user.serialize());
+		res.json({authToken})
+	}
+);
+
+router.post(
+	'/refresh',
+	passport.authenticate('jwt', {session: false}),
+	(req, res) => {
+		const authToken = createAuthToken(req.user);
+		res.json({authToken});
+	}
+);
+
+module.exports = {router};
--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 'use strict';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000'
 exports.DATABASE_URL = process.env.DATABASE_URL || 'http://localhost:8080';
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY;
