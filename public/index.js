2c67e1f0d1ba1ec5614e597f5cef234929d84e55
allyjfuller <allyjfuller@gmail.com>
2019-05-24 16:17:13 -0400

    index.js public

--- public/index.js
+++ public/index.js
@@ -323,3 +323,210 @@ function handleCityDelete() {
 			$(e.currentTarget).closest('.destinationCity').attr('data-id'));
 	});
 }
+
+function handleTravelDiaryDelete() {
+	$(".travelDiarySection").on('click', '.deleteTravelDiary', function(e) {
+		e.preventDefault();
+		deleteDiaryEntry(
+			$(e.currentTarget).closest('.diaryItem').attr('data-id'));
+	})
+}
+
+$(document).ready(function() {
+
+	$("#login-page").hide();
+	$("#register-page").hide();
+	$(".login-section").show();
+	$(".detail-section").show();
+
+	$("#login-button").click(function() {
+		$("#register-page").hide();
+		$(".login-section").hide();
+		$(".detail-section").hide();
+		$("#login-page").show();
+	})
+
+	$("#register-link").click(function() {
+		$("#login-page").hide();
+		$(".login-section").hide();
+		$(".detail-section").hide();
+		$("#register-page").show();
+	}) 
+
+	$("#register-button").click(function() {
+		$("#login-page").hide();
+		$(".login-section").hide();
+		$(".detail-section").hide();
+		$("#register-page").show();
+	})
+
+	$("#loginForm").submit(function(e) {
+		e.preventDefault();
+		let username = $("#GET-username").val();
+		let password = $("#GET-password").val();
+		let userInfo = {username, password};
+		let settings = {
+			url:"/auth/login",
+			type: 'POST',
+			contentType: 'application/json',
+			data: JSON.stringify(userInfo),
+			success: function(data) {
+				console.log('successfully logged in');
+				localStorage.setItem("authToken", data.authToken);
+				localStorage.setItem("currentUser", username);
+				user = username;
+				$("#login-page").hide();
+				$("#register-page").hide();
+				$(".login-section").hide();
+				$(".detail-section").hide();
+				$(".home").show();
+				$(".logout").show();
+				$(".tripDetails").show();
+				console.log(data);
+				getTrip(data);
+				getTravelDiary(data);
+			},
+			error: function(err) {
+				console.log(err);
+			}
+		};
+		$.ajax(settings);
+	})
+
+	$("#registerForm").submit(function(e) {
+		e.preventDefault();
+		let username = $("#POST-username").val();
+		console.log('client-side username is:', username);
+		let password = $("#POST-password").val();
+		let retypePass = $("#retype-password").val();
+		let user = {username, password};
+		let settings = {
+			url:"/users/",
+			type: 'POST',
+			contentType: 'application/json',
+			data: JSON.stringify(user),
+			success: function(data) {
+				console.log('successfully registered');
+				$("#registerForm input[type='text']").val('');
+				$("#register-page").hide();
+				$(".login-section").hide();
+				$(".detail-section").hide();
+				$("#login-page").show();
+			},
+			error: function(err) {
+				console.log(err);
+				if (password.length < 10) {
+					$("#errorTenChar").html("Password must be at least 10 characters")
+				}
+				if (password.length !== retypePass.length) {
+					$("#errorMatchPass").html("Passwords must match")
+				}
+				if (password !== retypePass) {
+					$("#errorMatchPass").html("Passwords must match")
+				}
+			}
+		};
+		$.ajax(settings);
+	})
+
+	$(".updateDestinationCitySection").hide();
+	$("#addDestinationCitySection").hide();
+	$(".destinationCitySection").show();
+
+	$("body").on("click", ".destinationCity", function() {
+		console.log("you clicked the destination");
+		event.preventDefault();
+		$(this).parent().find(".cityInfo").slideToggle(300);
+	});
+
+	$("body").on("click", ".travelDiaryDateAndTime", function() {
+		console.log("you clicked the date and time");
+		event.preventDefault();
+		$(this).parent().find(".travelDiaryInfo").slideToggle(300);
+	})
+
+	$("body").on("click", ".updateDestinationCity", function() {
+		console.log('you clicked update');
+		let city = $(this).parent().parent();
+		let id = $(this).parent().parent().attr("data-id");
+		console.log(id);
+		updateDestinationCity(id, city);
+	})
+
+	$("body").on("click", ".updateTravelDiary", function() {
+		console.log('you clicked update')
+		let diaryEntry = $(this).parent().parent();
+		console.log(diaryEntry);
+		let id = $(this).parent().parent().attr("data-id");
+		console.log(id);
+		updateTravelDiary(id, travelDiary);
+	})
+
+	$("body").on("submit", ".updateDestinationCity", function(e) {
+		e.preventDefault();
+		let id = $(this).attr("data-id")
+		console.log(`you submitted updateDestinationCity for ${id}`);
+		let updatedPlant = {
+			id: id,
+			destinationCity: $('.updateDestinationCity').val(),
+			travelDate: $('.updateTravelDate').val(),
+			busCompany: $('.updateBusCompany').val(),
+			comments: $('.updateComments').val(),
+		}
+		updateDestinationCity(id, updateDestinationCity);
+		console.log("city updated")
+	})
+
+	$("body").on("submit", ".updateTravelDiarySection", function(e) {
+		e.preventDefault();
+		let id = $(this).attr("data-id")
+		console.log(`you submitted updateTravelDiarySection for ${id}`);
+		let updateDiary = {
+			id: id,
+			content: $('.updateTravelDiary').val(),
+			datePublished: date.toDateString()
+		}
+		updateTravelDiary(id, updateDiary);
+		console.log("travel diary updated")
+	})
+
+	$("#cancel-add-city").click(function() {
+		$("#addDestinationCitySection input[type='text']").val('');
+		$("#addDestinationCitySection").hide();
+		$("#cancel-add-city").hide();
+	})
+
+	$("#add-city").click(function() {
+		$(".updateDestinationCitySection").hide();
+		$("#cancel-add-city").show();
+		$("#destinationCitySection").show();
+		$("#addDestinationCity").show();
+	})
+
+	$("#cancel-diary-entry").click(function() {
+		$("#addTravelDiarySection input[type='text']").val('');
+		$("#addTravelDiarySection").hide();
+		$("#cancel-diary-entry").hide();
+	})
+
+	$("#add-diary-entry").click(function() {
+		$("#cancel-diary-entry").show();
+		$("#addTravelDiarySection").show();
+	})
+
+	$(".logout").click(function() {
+		console.log('you clicked logout');
+		localStorage.clear();
+		user = null;
+		window.location.reload(true);
+	});
+
+	$(function() {
+		handleAddDestinationCity();
+		handleAddTravelDiary();
+		handleCityUpdate();
+		handleTravelDiaryDelete();
+		handleCityDelete();
+		handleTravelDiaryDelete();
+	});
+})
