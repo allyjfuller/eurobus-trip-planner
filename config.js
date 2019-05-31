c0f1ad8abd6b931f7fd9041bd404b4c7839fac79
allyjfuller <allyjfuller@gmail.com>
2019-05-31 10:38:02 -0400

    file changes 2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || '';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://admin:admin123@ds263436.mlab.com:63436/eurobus';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'secret'
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
